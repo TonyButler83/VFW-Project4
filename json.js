be48b8fce8f3cb6859b73ad2b01483ec50743fef
David Butler <davidbutler@David-Butlers-MacBook-Pro.local>
2012-02-23 00:48:45 -0500

    Added first files to the repo.

---
+++ json.js
@@ -0,0 +1,75 @@
+/*
+VFW Project Web App Part 4
+by: David Butler
+date: 2/23/2012
+term: 1202
+*/
+
+var json = {
+	"passwordEntry1": {
+		"group": ["Category:", "Email"],
+		"title": ["Title:", "Fullsail email account"],
+		"login": ["Login:", "tonyb@fullsail.com"],
+		"pword": ["Password:", "tbfso1"],
+		"cpword": ["Confirm Password:", "tbfso1"],
+		"sort": ["Sort By:", "Usage"],
+		"usage": ["Usage:", "1"],
+		"date": ["Date Modified:", "2012-02-09"],
+		"notes": ["Notes:", "vfw json test1"]
+	},
+	"passwordEntry2": {
+		"group": ["Category:", "Computer_Logins"],
+		"title": ["Title:", "MacBook Pro login"],
+		"login": ["Login:", "Tony's Mac"],
+		"pword": ["Password:", "tbfso2"],
+		"cpword": ["Confirm Password:", "tbfso2"],
+		"sort": ["Sort By:", "Date Added"],
+		"usage": ["Usage:", "3"],
+		"date": ["Date Modified:", "2012-02-16"],
+		"notes": ["Notes:", "vfw json test2"]
+	},
+	"passwordEntry3": {
+		"group": ["Category:", "Financial"],
+		"title": ["Title:", "Bank of America"],
+		"login": ["Login:", "TonyButler"],
+		"pword": ["Password:", "tbfso3"],
+		"cpword": ["Confirm Password:", "tbfso3"],
+		"sort": ["Sort By:", "Category"],
+		"usage": ["Usage:", "2"],
+		"date": ["Date Modified:", "2012-02-23"],
+		"notes": ["Notes:", "vfw json test3"]
+	},
+	"passwordEntry4": {
+		"group": ["Category:", "Online_Shopping"],
+		"title": ["Title:", "Amazon account"],
+		"login": ["Login:", "tonyb@yahoo.com"],
+		"pword": ["Password:", "tbfso4"],
+		"cpword": ["Confirm Password:", "tbfso4"],
+		"sort": ["Sort By:", "Usage"],
+		"usage": ["Usage:", "1"],
+		"date": ["Date Modified:", "2012-02-18"],
+		"notes": ["Notes:", "vfw json test4"]
+	},
+	"passwordEntry5": {
+		"group": ["Category:", "Other"],
+		"title": ["Title:", "Ps3 login"],
+		"login": ["Login:", "tonyb@gmail.com"],
+		"pword": ["Password:", "tbfso5"],
+		"cpword": ["Confirm Password:", "tbfso5"],
+		"sort": ["Sort By:", "Date Added"],
+		"usage": ["Usage:", "3"],
+		"date": ["Date Modified:", "2012-02-20"],
+		"notes": ["Notes:", "vfw json test5"]
+	},
+	"passwordEntry6": {
+		"group": ["Category:", "Personal"],
+		"title": ["Title:", "Work locker combination"],
+		"login": ["Login:", ""],
+		"pword": ["Password:", "0_13_20"],
+		"cpword": ["Confirm Password:", "0_13_20"],
+		"sort": ["Sort By:", "Title"],
+		"usage": ["Usage:", "2"],
+		"date": ["Date Modified:", "2012-02-17"],
+		"notes": ["Notes:", "vfw json test6"]
+	}
+}
